4f1e770011ae51bbac4aa19f1c9b0ae479f371d3
Ebuka007 <90689132+Ebuka007@users.noreply.github.com>
2021-09-14 13:23:25 +0100

    Add files via upload

--- session2.R
+++ session2.R
@@ -1,20 +1,76 @@
-hPrice <- c(5000000, 4500000, 3600000)
-hPrice
+x <- c("Apple", "Banana", "Pear","Mangoe","Pawpaw","Pineapple")
 
-hQuality <- c("Good", "Average", "Bad")
-hQuality
+str_sub(x, 1, 5)
+str_sub(x, -3, -2)
 
-#M <- c("hQuality,hPrice")
+x[1:4]
+x[-4:-1]
+
+a <- 100
+b <- 90
+
+a > b
+
+a <- 100
+b <- 93
+
+if (b > a) {
+  print ("b is greater than a")
+} else {
+  print("b is not greater than a")
+}
+
+a + b
+a / b
+a %% b
+
+a <- 20
+b <- 10
+
+if (b > a) {
+  print("b is greater than a")
+} else if (a == b) {
+  print("a and b are equal")
+} else {
+  print("a is greater than b")
+}
+
+a <- 20
+b <- 33
+c <- 10
+
+if (a > b & c > a){
+  print("Both conditions are true")
+}
 
 
-hQuality <- c("Good","Average","Bad","Good","Average","Bad","Bad","Good")
-hQuality <- factor(hQuality)
+a <- 200
+b <- 33
+c <- 500
+
+if (a > b | a > c){
+  print("At least one of the conditions is true")
+}
 
 
-hPrice <- c(5000000, 4500000, 3600000)
-hPrice
+my_cars <- list("bmw", "audi", "tesla", "honda","Toyota","G Wagon")
 
-hQuality <- c("Good", "Average", "Bad")
-hQuality
+for (x in my_cars) {
+  print(x)
+}
 
-DF <- data.frame("hQuality,hPrice")
+
+
+i <- 1
+while (i < 7) {
+  print(i)
+  i <- i + 1
+}
+
+
+
+
+
+
+
+
