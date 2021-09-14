d7de966bea2eb458157947717db5444fe31a367f
Ebuka007 <90689132+Ebuka007@users.noreply.github.com>
2021-09-14 11:05:29 +0100

    Add files via upload

---
+++ session2.R
@@ -0,0 +1,20 @@
+hPrice <- c(5000000, 4500000, 3600000)
+hPrice
+
+hQuality <- c("Good", "Average", "Bad")
+hQuality
+
+#M <- c("hQuality,hPrice")
+
+
+hQuality <- c("Good","Average","Bad","Good","Average","Bad","Bad","Good")
+hQuality <- factor(hQuality)
+
+
+hPrice <- c(5000000, 4500000, 3600000)
+hPrice
+
+hQuality <- c("Good", "Average", "Bad")
+hQuality
+
+DF <- data.frame("hQuality,hPrice")
